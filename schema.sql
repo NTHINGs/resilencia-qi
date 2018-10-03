08ee006a4c92fcc8f497adff78671f71e00f057c
Mauricio Martinez <n_othing@hotmail.com>
2018-10-03 15:49:08 -0500

    Agregando preguntas

--- schema.sql
+++ schema.sql
@@ -1,87 +1,51 @@
 -- ****************** RESILIENCIA QI ******************;
 -- ***************************************************;
-DROP TABLE %TABLE_PREFIX%resiliencia;
--- ************************************** %TABLE_PREFIX%pacientes
+DROP TABLE %TABLE_PREFIX%resultados;
 
-CREATE TABLE %TABLE_PREFIX%pacientes
+
+DROP TABLE %TABLE_PREFIX%preguntas;
+
+
+DROP TABLE %TABLE_PREFIX%factores_resiliencia;
+-- ************************************** %TABLE_PREFIX%preguntas
+
+CREATE TABLE %TABLE_PREFIX%preguntas
 (
-    id                 INT NOT NULL AUTO_INCREMENT,
-    fotografia         VARCHAR(500) NOT NULL ,
-    nombre             VARCHAR(100) NOT NULL ,
-    fechadenacimiento  DATE NOT NULL ,
-    edad               INT NOT NULL ,
-    escolaridad        VARCHAR(250) ,
-    ocupacion          VARCHAR(250) ,
-    estadocivil        VARCHAR(50) ,
-    cantidadhijos      INT ,
-    domicilio          VARCHAR(200) ,
-    ciudaddeorigen     VARCHAR(200) ,
-    telefono           VARCHAR(45) ,
-    email              VARCHAR(100) ,
-    enfermedades       TEXT ,
-    alergias           TEXT ,
-    responsable        VARCHAR(50) NOT NULL ,
-    fecha_creacion     DATETIME NOT NULL,
-    fecha_modificacion DATETIME,
+ id       INT NOT NULL ,
+ pregunta VARCHAR(200) NOT NULL ,
+ tipo     CHAR NOT NULL ,
+ grupo    VARCHAR(45) NOT NULL ,
+
+PRIMARY KEY (id),
+)%CHARSET_COLLATE%;
+
+
+-- ************************************** %TABLE_PREFIX%factores_resiliencia
+
+CREATE TABLE %TABLE_PREFIX%factores_resiliencia
+(
+ id                INT NOT NULL ,
+ nombre            VARCHAR(100) NOT NULL ,
+ fechadenacimiento DATE NOT NULL ,
+ edad              INT NOT NULL ,
+ fechaaplicacion   DATETIME NOT NULL ,
+
 PRIMARY KEY (id)
 )%CHARSET_COLLATE%;
 
 
--- ************************************** %TABLE_PREFIX%psicotropicos
+-- ************************************** %TABLE_PREFIX%resultados
 
-CREATE TABLE %TABLE_PREFIX%psicotropicos
+CREATE TABLE %TABLE_PREFIX%resultados
 (
-    id                 INT NOT NULL AUTO_INCREMENT,
-    sustancia          VARCHAR(45) NOT NULL ,
-    añoprimeruso       INT,
-    edadprimeruso      INT,
-    usoregular         VARCHAR(45),
-    unidadespordia     INT,
-    unidad             VARCHAR(45),
-    vecespordia        INT,
-    periodo            VARCHAR(45),
-    abstinenciamaxima  VARCHAR(45),
-    abstinenciaactual  VARCHAR(45),
-    viadeuso           VARCHAR(45),
-    fechaultimoconsumo DATE ,
-    paciente           INT NOT NULL ,
+ id           INT NOT NULL ,
+ respuesta    CHAR NOT NULL ,
+ pregunta     INT NOT NULL ,
+ cuestionario INT NOT NULL ,
 
 PRIMARY KEY (id),
-KEY fkIdx_67 (paciente),
-CONSTRAINT FK_67 FOREIGN KEY fkIdx_67 (paciente) REFERENCES %TABLE_PREFIX%pacientes (id)
+KEY fkIdx_19 (pregunta),
+CONSTRAINT FK_19 FOREIGN KEY fkIdx_19 (pregunta) REFERENCES %TABLE_PREFIX%preguntas (id) ON DELETE CASCADE ON UPDATE CASCADE,
+KEY fkIdx_22 (cuestionario),
+CONSTRAINT FK_22 FOREIGN KEY fkIdx_22 (cuestionario) REFERENCES %TABLE_PREFIX%factores_resiliencia (id) ON DELETE CASCADE ON UPDATE CASCADE
 )%CHARSET_COLLATE%;
-
-
--- ************************************** %TABLE_PREFIX%personas_contacto
-
-CREATE TABLE %TABLE_PREFIX%personas_contacto
-(
-    id        INT NOT NULL AUTO_INCREMENT,
-    nombre    VARCHAR(100) NOT NULL ,
-    relacion  VARCHAR(45) ,
-    domicilio VARCHAR(200) ,
-    telefonos VARCHAR(100) ,
-    paciente  INT NOT NULL ,
-
-PRIMARY KEY (id),
-KEY fkIdx_49 (paciente),
-CONSTRAINT FK_49 FOREIGN KEY fkIdx_49 (paciente) REFERENCES %TABLE_PREFIX%pacientes (id) ON DELETE CASCADE ON UPDATE CASCADE
-)%CHARSET_COLLATE%;
-
-
--- ************************************** %TABLE_PREFIX%riesgos_psicosociales
-
-CREATE TABLE %TABLE_PREFIX%riesgos_psicosociales
-(
-    id            INT NOT NULL AUTO_INCREMENT,
-    individual    TEXT ,
-    familiar      TEXT ,
-    entorno       TEXT ,
-    observaciones TEXT ,
-    paciente      INT NOT NULL ,
-
-PRIMARY KEY (id),
-KEY fkIdx_39 (paciente),
-CONSTRAINT FK_39 FOREIGN KEY fkIdx_39 (paciente) REFERENCES %TABLE_PREFIX%pacientes (id) ON DELETE CASCADE ON UPDATE CASCADE
-)%CHARSET_COLLATE%;
-
