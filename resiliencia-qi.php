ef0ad1db6ce05d50095b5c29c0ce2dc11770adf5
Mauricio Martinez <n_othing@hotmail.com>
2018-10-03 14:06:00 -0500

    Admin dashboard y formulario de registro de empresas

--- resiliencia-qi.php
+++ resiliencia-qi.php
@@ -29,25 +29,13 @@ if ( ! defined( 'ABS_VERSION_RESILENCIA' ) ) {
 	define( 'ABS_VERSION_RESILENCIA', '1.0.0' );
 }
 
-if ( ! defined( 'ABS_NAME_RESILENCIA' ) ) {
-	define( 'ABS_NAME_RESILENCIA', trim( dirname( plugin_basename( __FILE__ ) ), '/' ) );
-}
-
-if ( ! defined( 'ABS_DIR_RESILENCIA' ) ) {
-	define( 'ABS_DIR_RESILENCIA', WP_PLUGIN_DIR . '/' . ABS_NAME_RESILENCIA );
-}
-
-if ( ! defined( 'ABS_URL_resilencia' ) ) {
-	define( 'ABS_URL_resilencia', WP_PLUGIN_URL . '/' . ABS_NAME_RESILENCIA );
-}
-
 /**
  * Link.
  *
  * @since 1.0.0
  */
-if ( file_exists( ABS_DIR_RESILENCIA . '/shortcodes/formulario-registro-empresa.php' ) ) {
-	require_once( ABS_DIR_RESILENCIA . '/shortcodes/formulario-registro-empresa.php' );
+if ( file_exists( plugin_dir_path(__FILE__) . 'shortcodes/formulario-registro-empresa.php' ) ) {
+	require_once( plugin_dir_path(__FILE__) . 'shortcodes/formulario-registro-empresa.php' );
 }
 
 add_action('wp_enqueue_scripts','resiliencia_qi_init');
@@ -91,7 +79,7 @@ function render_resilencia_qi_admin() {
     print '<div class="wrap">';
 	print "<h1>$title</h1>";
 	print do_shortcode('[formulario-registro-empresa]');
-	print ABS_DIR_RESILENCIA . '/shortcodes/formulario-registro-empresa.php';
+	print plugin_dir_path(__FILE__) . 'shortcodes/formulario-registro-empresa.php';
 
     $file = plugin_dir_path( __FILE__ ) . "templates/admin.html";
 
