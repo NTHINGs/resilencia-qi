814efa405ffe8bde632f501d624c041158305c75
Mauricio Martinez <n_othing@hotmail.com>
2018-10-03 15:23:21 -0500

    Admin dashboard y formulario de registro de empresas

--- resiliencia-qi.php
+++ resiliencia-qi.php
@@ -81,7 +81,7 @@ function resiliencia_qi_admin() {
 function render_resiliencia_qi_admin() {
 	global $title;
 	$current_user = wp_get_current_user();
-	$link = array_pop(explode('/', $current_user->hash));
+	$link = get_user_meta($current_user->ID, 'hash', true);
 	
 	$variables = array(
 		"%TITLE%",
