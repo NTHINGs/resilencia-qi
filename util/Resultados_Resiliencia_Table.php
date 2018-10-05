81a048eae4e0fff3bfc110edbde0f6c88410eb7a
Mauricio Martinez <n_othing@hotmail.com>
2018-10-04 21:54:37 -0500

    Resultados del cuestionario

--- util/Resultados_Resiliencia_Table.php
+++ util/Resultados_Resiliencia_Table.php
@@ -22,7 +22,9 @@ class Resultados_Resiliencia_Table extends WP_List_Table {
         $hidden = $this->get_hidden_columns();
         $sortable = $this->get_sortable_columns();
         $data = $this->table_data();
-        usort( $data, array( &$this, 'sort_data' ) );
+		usort( $data, array( &$this, 'sort_data' ) );
+		/** Process bulk action */
+		$this->process_bulk_action();
         $perPage = $this->get_items_per_page( 'resultados_per_page', 10 );
         $currentPage = $this->get_pagenum();
         $totalItems = count($data);
@@ -42,11 +44,19 @@ class Resultados_Resiliencia_Table extends WP_List_Table {
     public function get_columns()
     {
         $columns = array(
-            'id'          => 'ID',
+			'cb'     	   => '<input type="checkbox" />',
+            'id'           => 'ID',
             'nombre'       => 'Nombre'
         );
         return $columns;
-    }
+	}
+	public function get_bulk_actions() {
+		$actions = [
+			'bulk-delete' => 'Eliminar'
+		];
+		
+		return $actions;
+	}
     /**
      * Define which columns are hidden
      *
@@ -63,7 +73,10 @@ class Resultados_Resiliencia_Table extends WP_List_Table {
      */
     public function get_sortable_columns()
     {
-        return array('nombre' => array('nombre', false));
+        return array(
+			'id' => array( 'id', true ),
+			'nombre' => array('nombre', false)
+		);
     }
     /**
      * Get the table data
@@ -76,7 +89,20 @@ class Resultados_Resiliencia_Table extends WP_List_Table {
 		$sql = "SELECT id, nombre FROM {$wpdb->prefix}resiliencia_registros";
 		$data = $wpdb->get_results( $sql, 'ARRAY_A' );
         return $data;
-    }
+	}
+	
+	function column_name( $item ) {
+		// create a nonce
+		$delete_nonce = wp_create_nonce( 'sp_delete_registro' );
+		
+		$title = '<strong>' . $item['nombre'] . '</strong>';
+		
+		$actions = [
+			'delete' => sprintf( '<a href="?page=%s&action=%s&registro=%s&_wpnonce=%s">Eliminar</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['id'] ), $delete_nonce )
+		];
+		
+		return $title . $this->row_actions( $actions );
+	}
     /**
      * Define what data to show on each column of the table
      *
@@ -121,5 +147,61 @@ class Resultados_Resiliencia_Table extends WP_List_Table {
             return $result;
         }
         return -$result;
-    }
+	}
+
+	public static function delete_registro( $id ) {
+		global $wpdb;
+		
+		$wpdb->delete(
+			"{$wpdb->prefix}resiliencia_registros",
+			[ 'id' => $id ],
+			[ '%d' ]
+		);
+	}
+	public function no_items() {
+		echo 'Nadie ha contestado aún.';
+	}
+
+	function column_cb( $item ) {
+		return sprintf(
+		  '<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['id']
+		);
+	}
+	
+	public function process_bulk_action() {
+		//Detect when a bulk action is being triggered...
+		if ( 'delete' === $this->current_action() ) {
+	  
+		  // In our file that handles the request, verify the nonce.
+		  $nonce = esc_attr( $_REQUEST['_wpnonce'] );
+	  
+		  if ( ! wp_verify_nonce( $nonce, 'sp_delete_registro' ) ) {
+			die( 'Go get a life script kiddies' );
+		  }
+		  else {
+			self::delete_registro( absint( $_GET['registro'] ) );
+	  
+			wp_redirect( esc_url( add_query_arg() ) );
+			exit;
+		  }
+	  
+		}
+	  
+		// If the delete bulk action is triggered
+		if ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-delete' )
+			 || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-delete' )
+		) {
+	  
+		  $delete_ids = esc_sql( $_POST['bulk-delete'] );
+	  
+		  // loop over the array of record IDs and delete them
+		  foreach ( $delete_ids as $id ) {
+			self::delete_registro( $id );
+	  
+		  }
+	  
+		  wp_redirect( esc_url( add_query_arg() ) );
+		  exit;
+		}
+	}
 }
