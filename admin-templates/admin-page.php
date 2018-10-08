c46156993ecfc4d0d890b3043c58b7c0713d6f6b
Mauricio Martinez <n_othing@hotmail.com>
2018-10-08 14:37:49 -0500

    Agregando promedios de tu organizacion

--- admin-templates/admin-page.php
+++ admin-templates/admin-page.php
@@ -51,7 +51,7 @@ function render_resiliencia_qi_admin() {
             get_site_url(),
             get_user_hash(),
         );
-        print str_replace($variables, $values, file_get_contents(  RES_PLUGIN_PATH . "templates/resultados-organizacion.html" ));
+        // print str_replace($variables, $values, file_get_contents(  RES_PLUGIN_PATH . "templates/resultados-organizacion.html" ));
         print do_shortcode('[resultados-cuestionario org_id="' . get_user_hash() . '"]');
         // print '</div></div>';
         render_table_resultados(false);
