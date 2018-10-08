ab348cab2c450c8fc5fc350bcc76ac313288417a
Mauricio Martinez <n_othing@hotmail.com>
2018-10-08 16:00:41 -0500

    Agregando promedios de tu organizacion

--- util/Orgs_Resiliencia_Table.php
+++ util/Orgs_Resiliencia_Table.php
@@ -27,6 +27,9 @@ class Orgs_Resiliencia_Table extends WP_List_Table {
 		usort( $data, array( &$this, 'sort_data' ) );
 		$this->_column_headers = array($columns, $hidden, $sortable);
 
+		/** Acciones en lote */
+		$this->process_bulk_action();
+
 		// Paginacion
         $perPage = $this->get_items_per_page( 'resultados_per_page', 10 );
         $currentPage = $this->get_pagenum();
@@ -121,7 +124,7 @@ class Orgs_Resiliencia_Table extends WP_List_Table {
 		$title = '<strong>' . $item['nombre'] . '</strong>';
 	  
 		$actions = [
-			'view'    => sprintf( '<a href="?page=%s&org_id=%s">Ver</a>', $_REQUEST['page'], $item['id'] ),
+			'view'    => sprintf( '<a href="?page=%s&action=%s&org_id=%s&noheader=true">Ver</a>', $_REQUEST['page'], 'view', $item['id'] ),
 		];
 	  
 		return $title . $this->row_actions( $actions );
@@ -181,5 +184,13 @@ class Orgs_Resiliencia_Table extends WP_List_Table {
      */
 	public function no_items() {
 		echo 'No hay resultados.';
+	}
+	
+	public function process_bulk_action() {
+		//Detect when a bulk action is being triggered...
+		if ( 'view' === $this->current_action() ) {
+			wp_redirect(add_query_arg( 'org_id', $_GET['org_id'], admin_url('admin.php?page=resultados-organizacionales') ));
+			exit;
+		}
 	}
 }
